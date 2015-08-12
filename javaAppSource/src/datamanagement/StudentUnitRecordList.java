69100c02a20378a91981d14ff940de2c9c7f9395
mb <edu.markb@gmail.com>
2015-08-12 13:34:17 -0400

    Modified the comments in these files

--- javaAppSource/src/datamanagement/StudentUnitRecordList.java
+++ javaAppSource/src/datamanagement/StudentUnitRecordList.java
@@ -2,8 +2,5 @@ package datamanagement;
 public class StudentUnitRecordList
     extends java.util.ArrayList<IStudentUnitRecord>
 {
-  /**
-   * 
-   */
   private static final long serialVersionUID = 1L;
 }
