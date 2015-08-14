cea3872dce1a770b54124f6e155ca6b037f00018
Andrew <andrewmallitt@gmail.com>
2015-08-14 22:22:08 +1000

    Fix error in syncing

--- javaAppSource/src/datamanagement/StudentUnitRecord.java
+++ javaAppSource/src/datamanagement/StudentUnitRecord.java
@@ -1,37 +1,67 @@
 package datamanagement;
-public class StudentProxy implements IStudent {
-	private Integer I;
-	private String l;
-	private String Il;
-	private StudentManager lI;
-	public StudentProxy(Integer id, String fn, String Il) {
-		this.I = id;
-		this.l = fn;
-		this.Il = Il;
-		this.lI = StudentManager.get();
+
+public class StudentUnitRecord implements IStudentUnitRecord {
+	private Integer sid;
+	private String uc;
+	private float a1, a2, ex;
+
+	public StudentUnitRecord(Integer id, String code, float asg1, float asg2,
+			float exam) {
+		this.sid = id;
+		this.uc = code;
+		this.setAsg1(asg1);
+		this.setAsg2(asg2);
+		this.setExam(exam);
 	}
-	public Integer getID() {
-		return I;
+
+	public Integer getStudentID() {
+		return sid;
 	}
-	public String getFirstName() {
-		return l;
+
+	public String getUnitCode() {
+		return uc;
 	}
-	public String getLastName() {
-		return Il;
+
+	public void setAsg1(float a1) {
+		if (a1 < 0 ||
+			a1 > UnitManager.UM().getUnit(uc).getAsg1Weight()) {
+			throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
+		}
+		this.a1 = a1;
 	}
-	public void setFirstName(String firstName) {
-		lI.getStudent(I).setFirstName(firstName);
+
+	public float getAsg1() {
+
+		return a1;
 	}
-	public void setLastName(String lastName) {
-		lI.getStudent(I).setLastName(lastName);
+
+	public void setAsg2(float a2) {
+		if (a2 < 0 ||
+			a2 > UnitManager.UM().getUnit(uc).getAsg2Weight()) {
+			throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
+		}
+		this.a2 = a2;
+
 	}
-	public void addUnitRecord(IStudentUnitRecord record) {
-		lI.getStudent(I).addUnitRecord(record);
+
+	public float getAsg2() {
+		return a2;
 	}
-	public IStudentUnitRecord getUnitRecord(String unitCode) {
-		return lI.getStudent(I).getUnitRecord(unitCode);
+
+	public void setExam(float ex) {
+		if (ex < 0 ||
+				ex > UnitManager.UM().getUnit(uc).getExamWeight()) {
+				throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
+			}
+		this.ex = ex;
 	}
-	public StudentUnitRecordList getUnitRecords() {
-		return lI.getStudent(I).getUnitRecords();
+
+	public float getExam() {
+		return ex;
+	}
+
+	public float getTotal() {
+		return a1 + a2 + ex;
+
 	}
 }
